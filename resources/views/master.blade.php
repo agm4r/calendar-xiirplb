6acd44575613db21f50224cd694be63327a6fc1d
agm4r <agmarputra@gmail.com>
2021-03-24 18:30:34 +0700

    memindahkan link js ke bawah

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,9 +9,6 @@
 
    <!-- Custome CSS -->
    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-
-   <!-- JS -->
-   <script src="{{ asset('js/app.js') }}" defer></script>
   <!--  welcome css -->
     <title>@yield('title')</title>
   </head>
@@ -83,6 +80,12 @@
 </footer>
 <!-- Akhir Footer -->
 
+<!-- JS -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+<script src="{{ asset('js/script.js') }}"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
   </body>
 </html>
